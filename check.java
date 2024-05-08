a9e70c920255436232bf639f4c816af47d94aa46
dhrithi09 <106476416+dhrithi09@users.noreply.github.com>
2024-05-08 15:08:06 +0530

    Update3 check.java

--- check.java
+++ check.java
@@ -1,8 +1,6 @@
 import java.util.*;
-class Test
-{
-    public static void main(String []args)
-    {
+class Test{
+    public static void main(String[]args){
         System.out.println("Testing if workflow throws error. Because I am purposely leaving out the semicolon");
     }
 }
