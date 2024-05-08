c39a83c5b2a53b9f710547da8a3bf8e690b777ae
dhrithi09 <106476416+dhrithi09@users.noreply.github.com>
2024-05-08 14:58:35 +0530

    Update check.java

--- check.java
+++ check.java
@@ -2,6 +2,6 @@ class Test
 {
     public static void main(String []args)
     {
-        System.out.println("Testing if workflow throws error. Because I am purposely leaving out the semicolon")
+        System.out.println("Testing if workflow throws error. Because I am purposely leaving out the semicolon");
     }
 }
